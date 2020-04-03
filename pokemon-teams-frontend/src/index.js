39ee33329e1b987ab894378e60ede5bd4b2e36af
Naya Willis <greedybrain18@gmail.com>
2020-04-03 11:08:58 -0400

    pokemon learn lab code

--- pokemon-teams-frontend/src/index.js
+++ pokemon-teams-frontend/src/index.js
@@ -1,3 +1,42 @@
-const BASE_URL = "http://localhost:3000"
-const TRAINERS_URL = `${BASE_URL}/trainers`
-const POKEMONS_URL = `${BASE_URL}/pokemons`
+const BASE_URL = "http://localhost:3000";
+const TRAINERS_URL = `${BASE_URL}/trainers`;
+const POKEMONS_URL = `${BASE_URL}/pokemons`;
+const main = document.querySelector("main");
+
+document.addEventListener("DOMContentLoaded", e => {
+     // debugger;
+     getTrainers();
+});
+
+const getTrainers = () => {
+     return fetch(TRAINERS_URL)
+          .then(resp => resp.json())
+          .then(trainerData => {
+               createCards(trainerData);
+               debugger;
+               console.log("SUCCESS");
+          })
+          .catch(err => console.log(err.message));
+}
+
+const createCards = trainerData => {
+     trainerData.forEach(trainer => {
+          let trainerCard = document.createElement("div");
+          trainerCard.classList.add("card");
+          trainerCard.setAttribute("data-id", `${trainer.id}`);
+          trainerCard.innerHTML += `
+          <p>${trainer.name}</p>
+          <button data-trainer-id="${trainer.id}">Add Pokemon</button>
+       `;
+
+          let pokemonHolder = document.createElement("ul");
+
+          trainer.pokemons.forEach(pokemon => {
+               pokemonHolder.innerHTML += `
+          <li>${pokemon.nickname} (${pokemon.species}) <button class="release" data-pokemon-id="${pokemon.id}">Release</button></li>
+          `;
+               trainerCard.append(pokemonHolder);
+               main.appendChild(trainerCard);
+          });
+     });
+};
