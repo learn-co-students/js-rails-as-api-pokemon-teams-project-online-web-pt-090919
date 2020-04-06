dcd415b0fc1767fade23a37cea3c0f34e330dddb
Naya Willis <greedybrain18@gmail.com>
2020-04-05 20:42:23 -0400

    Done.

--- pokemon-teams-frontend/src/index.js
+++ pokemon-teams-frontend/src/index.js
@@ -4,29 +4,38 @@ const POKEMONS_URL = `${BASE_URL}/pokemons`;
 const main = document.querySelector("main");
 
 document.addEventListener("DOMContentLoaded", e => {
-     // debugger;
      getTrainers();
+     postPokemon();
+     removePokemon()
 });
 
-const getTrainers = () => {
-     return fetch(TRAINERS_URL)
-          .then(resp => resp.json())
-          .then(trainerData => {
-               createCards(trainerData);
-               debugger;
-               console.log("SUCCESS");
-          })
-          .catch(err => console.log(err.message));
+
+// GET TRAINERS 
+function getTrainers() {
+     setUpAllTrainersForRetrieval();
 }
 
-const createCards = trainerData => {
+// POST A POKEMON
+function postPokemon() {
+     setUpPokemonForCreation();
+}
+
+// REMOVE A POKEMON
+function removePokemon() {
+     setUpPokemonForDeletion();
+}
+
+// ============================================================
+
+// CREATE TRAINERS
+function createTrainerCards(trainerData) {
      trainerData.forEach(trainer => {
           let trainerCard = document.createElement("div");
           trainerCard.classList.add("card");
           trainerCard.setAttribute("data-id", `${trainer.id}`);
           trainerCard.innerHTML += `
           <p>${trainer.name}</p>
-          <button data-trainer-id="${trainer.id}">Add Pokemon</button>
+          <button data-trainer-id="${trainer.id}" class="add-pokemon-btn">Add Pokemon</button>
        `;
 
           let pokemonHolder = document.createElement("ul");
@@ -40,3 +49,103 @@ const createCards = trainerData => {
           });
      });
 };
+// ============================================================
+
+// GET ALL POKEMON 
+function setUpAllTrainersForRetrieval() {
+     fetch(TRAINERS_URL)
+          .then(res => res.json())
+          .then(trainerData => {
+               createTrainerCards(trainerData);
+               console.log("SUCCESS");
+          })
+          .catch(err => console.log(err.message));
+}
+// ============================================================
+
+// ADD A POKEMON
+function setUpPokemonForCreation() {
+     main.addEventListener('click', e => {
+          let addButtons = document.querySelectorAll('.add-pokemon-btn') // all add pokemon buttons
+          let pokemonHolder = e.target.nextElementSibling
+          addButtons.forEach(btn => {
+               if (e.target === btn) {
+                    let trainerId = btn.parentElement.dataset.id;
+                    let pokemonList = btn.nextElementSibling.querySelectorAll('li');
+
+                    if (pokemonList.length >= 6) {
+                         alert("You can't add any more pokemon")
+                    } else {
+                         createFetchPost(trainerId)
+                    }
+               }
+          })
+
+          // =====================================================
+
+          // ABSTRACTING AWAY POST FETCH CODE
+          function createFetchPost(id) {
+               data = {
+                    "trainer_id": id
+               }
+
+               options = {
+                    "method": "POST",
+                    "headers": {
+                         'Content-Type': "application/json",
+                         'Accept': "application/json"
+                    },
+                    body: JSON.stringify(data)
+               }
+
+               fetch(POKEMONS_URL, options)
+                    .then(res => res.json())
+                    .then(pokemon => {
+                         createPokemonItem(pokemon)
+                         console.log(data)
+                    })
+                    .catch(err => console.log(err.message))
+
+               // ==============================================
+
+               function createPokemonItem(pokemon) {
+                    pokemonItem = document.createElement('li')
+                    pokemonItem.innerHTML += `${pokemon.data.nickname} (${pokemon.data.species})
+                    <button class="release" data-pokemon-id="${pokemon.data.id}"> Release</button>`
+                    pokemonHolder.appendChild(pokemonItem)
+               }
+               // ===============================================
+          }
+          // =====================================================
+     })
+}
+
+// ============================================================
+
+// DELETE A POKEMON 
+function setUpPokemonForDeletion() {
+     main.addEventListener('click', e => {
+          let releaseButtons = document.querySelectorAll('.release') // all release pokemon buttons
+          releaseButtons.forEach(btn => {
+               if (e.target === btn) {
+                    let pokemonId = btn.dataset.pokemonId;
+                    createFetchDelete(pokemonId)
+                    btn.parentElement.remove()
+               }
+          })
+
+          // ====================================================
+
+          // ABSTRACTING AWAY DELETE FETCH CODE
+          function createFetchDelete(id) {
+               fetch(`${POKEMONS_URL}/${id}`, {
+                         "method": "DELETE"
+                    })
+                    .then(console.log("Deleted Successfully"))
+                    .catch(err => console.log(err.message))
+          }
+          // =====================================================
+     })
+}
+
+// ============================================================
