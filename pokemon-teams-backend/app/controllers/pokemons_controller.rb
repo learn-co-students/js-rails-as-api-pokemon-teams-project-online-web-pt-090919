f5ebbaf334f9e7976f865960439c5c2aff9ca3fe
kalmegati <kalmegati@gmail.com>
2020-04-10 18:11:57 -0400

    Done.

--- pokemon-teams-backend/app/controllers/pokemons_controller.rb
+++ pokemon-teams-backend/app/controllers/pokemons_controller.rb
@@ -1,2 +1,22 @@
 class PokemonsController < ApplicationController
+
+    def create
+        pokemon = Pokemon.new(
+            species: Faker::Games::Pokemon.name,
+            nickname: Faker::Name.first_name,
+            trainer_id: pokemon_params[:trainer_id]
+        )
+        pokemon.save
+    end
+
+    def destroy
+
+    end
+
+    private
+
+    def pokemon_params
+        params.require("pokemon").permit("trainer_id")
+    end
+
 end
--- pokemon-teams-frontend/src/index.js
+++ pokemon-teams-frontend/src/index.js
@@ -4,14 +4,37 @@ const POKEMONS_URL = `${BASE_URL}/pokemons`
 
 const main = document.querySelector("main");
 
-function gettoDaze(number) {
+document.addEventListener("DOMContentLoaded", function() {
+    gettoDaze();
+});
+
+let league = []
+
+function updateLeague() {
+    league.length = 0;
     fetch(TRAINERS_URL)
     .then(function(response) {
         return response.json();
     })
     .then(function(json) {
-        let trainer = json[`${number}`];
-        trainerCard(trainer);
+        for (let trainer of json) {
+            league.push(trainer);
+        };
+    });
+}
+
+function gettoDaze() {
+    fetch(TRAINERS_URL)
+    .then(function(response) {
+        return response.json();
+    })
+    .then(function(json) {
+        for (let trainer of json) {
+            league.push(trainer);
+        };
+        for (let trainer of league) {
+            trainerCard(trainer);
+        };
     });
 };
 
@@ -22,7 +45,9 @@ function trainerCard(trainer) {
     let p = document.createElement("p");
         p.textContent = trainer.name;
     let addButton = document.createElement("button");
+        addButton.setAttribute("class", "tester")
         addButton.setAttribute("data-trainer-id", trainer.id);
+        addButton.addEventListener("click", addPokemon);
         addButton.textContent = "Add Pokemon";
     let ul = document.createElement("ul");
     for (let pokemon of trainer.pokemons) {
@@ -40,3 +65,51 @@ function trainerCard(trainer) {
     div.appendChild(ul);
     main.appendChild(div);
 };
+
+function addPokemon() {
+    console.log("Suc");
+    let formData = {
+        trainer_id: this.getAttribute("data-trainer-id")
+    };
+       
+    let configObj = {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+          "Accept": "application/json"
+        },
+        body: JSON.stringify(formData)
+    };
+    
+    let trainerCard = this.parentElement;
+
+    fetch("http://localhost:3000/pokemons", configObj)
+    .then(function(response) {
+        addPokeLine(trainerCard.getAttribute("data-id"));
+    })
+
+};
+
+function addPokeLine(index) {
+    fetch(TRAINERS_URL)
+    .then(function(response) {
+        return response.json();
+    })
+    .then(function(json) {
+        let trainer = json[`${index - 1}`];
+        let pokeLine = makePokeLine(trainer.pokemons[`${trainer.pokemons.length - 1}`]);
+        let ul = document.querySelectorAll("ul")[`${index - 1}`];
+        ul.appendChild(pokeLine);
+    });
+}
+
+function makePokeLine(pokemon) {
+    let li = document.createElement("li");
+        li.textContent = `${pokemon.nickname} (${pokemon.species})`;
+    let remButton = document.createElement("button");
+        remButton.setAttribute("class", "release");
+        remButton.setAttribute("data-pokemon-id", pokemon.id);
+        remButton.textContent = "Release";
+    li.appendChild(remButton);
+    return li
+}
